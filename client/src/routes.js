ee8668a5905807fd3aaa5cbf6ad535cdce21c620
Ben Heil <ben.jer.heil@gmail.com>
2017-12-28 15:45:01 -0600

    Added some more files

---
+++ client/src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+
+import Home from '../pages/Home.js';
+import ScoringPage from '../pages/ScoringPage.js';
+import BenPage from '../pages/BenPage.js';
+import ChallengePage from '../pages/ChallengePage.js'
+
+
+export default class Routes extends React.Component {
+	render() {
+		return (
+			<HashRouter>
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/scoring" component={ScoringPage} />
+      		<Route exact path="/test" component={BenPage} />
+          <Route path='/challenges' component={ChallengePage} />
+
+				</div>
+			</HashRouter>
+		);
+	}
+}
